a85fff3a32664249781550067478a481d354d56e
Freedisch <freeproduc@gamil.com>
2022-04-13 00:22:12 +0200

    Putchar level

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - file
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	void print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+
+void print_alphabet(void)
+{
+	char word;
+	for(word='a';word<='z';word++){
+		putchar(ch);
+	}
+
+	return(0);
+}
 #endif
