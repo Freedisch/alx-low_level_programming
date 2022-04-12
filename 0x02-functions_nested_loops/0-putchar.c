8e1d53cbe54182b3a9fd9a428d48199ffa55a8ad
Freedisch <freeproduc@gamil.com>
2022-04-12 23:55:46 +0200

    Putchar level

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * Main file
+ */
+
+int main(){
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return 0;
+}
+
+
+
