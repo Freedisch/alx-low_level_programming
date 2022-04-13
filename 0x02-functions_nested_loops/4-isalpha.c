0b920b35db2c5d7f13f755bbb30ea694fc341bab
Freedisch <freeproduc@gamil.com>
2022-04-13 22:34:50 +0200

    Putchar level

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *_isalpha - return 1 if c is a lower or uppercase
+ *@c: character check
+ *Return - 1 or 0
+ */
+
+int main()
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
+
