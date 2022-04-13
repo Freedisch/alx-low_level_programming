ace5ac83507137c414759f761c82c7ce1f2f1cac
Freedisch <freeproduc@gamil.com>
2022-04-13 22:28:08 +0200

    Putchar level

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * islower - if a letter is a lower case or not
+ * _islower - if a letter is a lower case or not
  * @c: the character to check
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	return(c >= 'a' && c <= 'z');
+	return (c >= 'a' && c <= 'z');
 }
 
