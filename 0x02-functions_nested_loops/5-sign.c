f19a2bdd75c098f117b676b0fa9b2a10c5139139
Freedisch <freeproduc@gamil.com>
2022-04-13 23:02:34 +0200

    Putchar level

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,13 +7,16 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-		print("+");
+	if (n > 0) {
+		putchar("+");
 		return (1);
-	else if (n = 0)
-		print("0");
+	}
+	else if (n = 0) {
+		putchar("0");
 		return (0);
-	else:
-		print("-");
+	}
+	else {
+		putchar("-");
 		return (-1);
+	}
 }
