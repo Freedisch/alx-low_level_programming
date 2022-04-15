83eef8fb7a44dacb44c378ee6a347a5cd0376cb1
Freedisch <freeproduc@gamil.com>
2022-04-15 14:38:29 +0200

    Putchar level

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_last_digit - hehehe
+ * Return: hfhfhfhf
+ * @n: jfjfjf
+ **/
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+	putchar(a + '0');
+
+	return (a);
+}
+
+
+	
